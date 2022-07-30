965ccab7e19c01d266b180b1a571b4f3bffa23cb
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-30 10:51:02 -0700

    Add header and nav

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+  router.get('/', (req, res) => {
+    db.query('SELECT * FROM users;')
       .then(data => {
         const users = data.rows;
         res.json({ users });
