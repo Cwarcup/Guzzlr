d9083edc839ce90f1c52140f626e5dbd3721d36d
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-30 13:50:53 -0700

    Login added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,41 @@
+/* eslint-disable no-undef */
 // Client facing scripts here
 $(function () {
+  
+  // login html
+  const login = () => {
+    return (
+      `
+      <div class='login-container'>
+        <h1>Login</h1>
+        <form action="/login" method="POST">
+          <div class="">
+            <label class="" for="email">Email address</label>
+            <input name="email" type="email" class="" placeholder="Enter email" id="email" />
+          </div>
 
-  $('body').click(() => {
-    console.log('body clicked');
+          <div class="">
+            <label class="" for="password">Password</label>
+            <input name="password" type="password" class="" placeholder="Password" id="password" />
+          </div>
+
+          <div class="">
+            <button type="submit" class="">Login
+            </button>
+          </div>
+        </form>
+      </div>
+      `
+    );
+  };
+
+  $('.login').click((event) => {
+    event.preventDefault();
+    $('.header-welcome').hide();
+    $('.menu-options-container').hide();
+    $('.previous-orders-container').hide();
+    const loginHTML = login();
+    $('#main-cont').append(loginHTML);
+
   });
 });
