f41993ba41fc911f3f21c2d428d4657b9175efd9
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-30 19:23:30 -0700

    Add max code

--- routes/userOrderHistory.js
+++ routes/userOrderHistory.js
@@ -8,13 +8,26 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     db.query(
       `
-      SELECT
-        *
-      FROM
-        orders
-        JOIN users ON user_id = users.id
-      WHERE
-        user_id = 1;
+    SELECT
+      orders.order_placed,
+      restaurants.name,
+      orders.price,
+      menu_items.name,
+      count(menu_items.name)
+    FROM
+      orders
+      JOIN users ON user_id = users.id
+      JOIN restaurants ON restaurant_id = restaurants.id
+      JOIN order_items ON order_id = orders.id
+      JOIN menu_items ON order_items.menu_item = menu_items.id
+    WHERE
+      user_id = 1
+    GROUP BY
+      orders.id,
+      restaurants.name,
+      orders.order_placed,
+      menu_items.name,
+      orders.price;
   `)
       .then(data => {
         const userOrderHistory = data.rows;
