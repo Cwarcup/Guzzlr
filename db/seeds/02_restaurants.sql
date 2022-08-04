f96399cef4e23a37df0faae9668d5fc21cf72f6e
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-04 16:25:47 -0700

    Seeds updated

--- db/seeds/02_restaurants.sql
+++ db/seeds/02_restaurants.sql
@@ -1,2 +1,28 @@
 -- Restaurants Seeds
-INSERT INTO restaurants (owner_id, name, cuisine_type, phone_number, country, street, city, province, post_code) VALUES (3, 'MacDondal Hamberders', 'Fast Food', '867-5309', 'Canada', '123 Sesame St', 'Victoria', 'BC', 'A2C 4E6');
+INSERT INTO
+  restaurants (
+    owner_id,
+    name,
+    cuisine_type,
+    phone_number,
+    country,
+    street,
+    city,
+    province,
+    post_code
+  )
+VALUES
+  (
+    3,
+    'McDonalds',
+    'Fast Food',
+    '867-5309',
+    'Canada',
+    '123 Sesame St',
+    'Victoria',
+    'BC',
+    'A2C 4E6'
+  );
+
+-- restaurant owner is Adam C. Max
+-- email is adam@gmail.com
--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,82 +1,78 @@
 -- Orders table seeds here (Example)
-INSERT INTO
-  orders (
-    user_id,
-    restaurant_id,
-    order_placed,
-    notes,
-    price
-  )
-VALUES
-  (
-    1,
-    1,
-    '2015-03-08 02:00:00',
-    'Im pending',
-    2000
-  );
-
-INSERT INTO
-  orders (
-    user_id,
-    restaurant_id,
-    order_placed,
-    notes,
-    price
-  )
-VALUES
-  (
-    1,
-    1,
-    '2015-03-08 04:00:00',
-    'I am pending',
-    2000
-  );
-
--- current order
--- order_started is NOT null
-INSERT INTO
-  orders (
-    user_id,
-    restaurant_id,
-    order_placed,
-    order_started,
-    notes,
-    price,
-    pickup_time
-  )
-VALUES
-  (
-    2,
-    1,
-    '2015-03-08 04:00:00',
-    '2015-03-08 04:01:00',
-    'I am a current order',
-    5000,
-    '6:00 PM'
-  );
-
--- completed order
--- should not show up on admin
-INSERT INTO
-  orders (
-    user_id,
-    restaurant_id,
-    order_placed,
-    order_started,
-    order_completed,
-    notes,
-    price,
-    pickup_time
-  )
-VALUES
-  (
-    5,
-    1,
-    '2015-03-08 02:00:00',
-    '2015-03-08 02:11:00',
-    '2015-03-08 02:30:00',
-    'Should not show up on admin',
-    8000,
-    '6:00 PM'
-  );
+-- two pending orders
+-- INSERT INTO
+--   orders (
+--     user_id,
+--     restaurant_id,
+--     order_placed,
+--     notes,
+--     price
+--   )
+-- VALUES
+--   (
+--     1,
+--     1,
+--     '2022-08-05 17:02:00',
+--     'Please make sure to bring a large plate for the entree.',
+--     45000
+--   );
+-- INSERT INTO
+--   orders (
+--     user_id,
+--     restaurant_id,
+--     order_placed,
+--     notes,
+--     price
+--   )
+-- VALUES
+--   (
+--     20,
+--     1,
+--     '2022-08-05 17:10:00',
+--     'Extra napkins please.',
+--     1875
+--   );
+-- -- current order
+-- -- order_started is NOT null
+-- INSERT INTO
+--   orders (
+--     user_id,
+--     restaurant_id,
+--     order_placed,
+--     order_started,
+--     price,
+--     pickup_time
+--   )
+-- VALUES
+--   (
+--     2,
+--     1,
+--     '2022-08-05 16:52',
+--     '2022-08-05 16:53',
+--     7250,
+--     '10:15 AM'
+--   );
+-- -- completed order
+-- -- should not show up on admin
+-- INSERT INTO
+--   orders (
+--     user_id,
+--     restaurant_id,
+--     order_placed,
+--     order_started,
+--     order_completed,
+--     notes,
+--     price,
+--     pickup_time
+--   )
+-- VALUES
+--   (
+--     5,
+--     1,
+--     '2015-03-08 02:00:00',
+--     '2015-03-08 02:11:00',
+--     '2015-03-08 02:30:00',
+--     'Should not show up on admin',
+--     8000,
+--     '6:00 PM'
+--   );
